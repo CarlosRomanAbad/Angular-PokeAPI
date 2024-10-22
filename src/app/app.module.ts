8fb1fa184987991d60f1549886483a29978a8a84
carlosromanabad <carlosromanabad@gmail.com>
2024-10-22 13:29:42 +0200

    Add PokeListComponent to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PokeListComponent } from './components/poke-list/poke-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokeListComponent
   ],
   imports: [
     BrowserModule,
