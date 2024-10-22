7a33acd43c4b543ac1364129990e51263317d06c
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-10-22 21:27:48 +0200

    feat- se listan los pokemon / se añade header "PokeApi"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { provideHttpClient } from '@angular/common/http';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    HeaderComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -16,7 +23,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgbModule
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon, PokemonResponse } from '../../interfaces/ipokemon';
+import { PokemonServiceService } from '../../services/pokemon-service.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrl: './pokemon-list.component.css'
+})
+export class PokemonListComponent implements OnInit{
+
+
+pokemonList: Pokemon[] = [];
+
+constructor(private pokemonService: PokemonServiceService ) { }
+
+ngOnInit(): void {
+
+  this.pokemonService.getPokemonList(12).subscribe((x: PokemonResponse) => {
+
+    this.pokemonList = x.results;
+
+  });
+
+}
+
+getPokemonId(url: string): number{
+  
+  return this.pokemonService.getPokemonId(url);
+
+  }
+  createImgUrl(id: number): string{
+
+  return this.pokemonService.createImgUrl(id);
+
+  }
+
+
+}
