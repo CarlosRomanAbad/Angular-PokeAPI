6f1ded0e43fc970429c36a5f329d448c29857ce4
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-10-23 12:28:49 +0200

    para casa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
 
 const routes: Routes = [
 
-{ path : 'pokemon' , component : PokemonListComponent},
+{ path : 'pokemon' , component : PokemonListComponent },
+{ path : 'pokemon/:id', component : PokemonDetailsComponent },
 { path : '' , redirectTo : '/pokemon', pathMatch : 'full' },
 { path : '**', component : PageNotFoundComponent }
 
