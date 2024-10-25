6feab23c64dcd414b10be3867775902b625683ff
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-10-25 10:50:16 +0200

    feat- boton terminado

---
+++ src/app/components/nav-to-top-button/nav-to-top-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-to-top-button',
+  templateUrl: './nav-to-top-button.component.html',
+  styleUrl: './nav-to-top-button.component.css'
+})
+export class NavToTopButtonComponent {
+
+}
